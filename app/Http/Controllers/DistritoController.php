043a25b93075332753f0ebb58f476f10a4b779dd
Julio Cesar <jccintr@gmail.com>
2023-11-17 15:53:21 -0300

    sksk

--- app/Http/Controllers/DistritoController.php
+++ app/Http/Controllers/DistritoController.php
@@ -68,7 +68,9 @@ class DistritoController extends Controller
         
         $stake = $request->stake * 1;
         $season = $request->season * 1;
+        $home = $request->home;
         
+
         $response = Http::get('https://api.football-data-api.com/league-teams?key=example&season_id='.$season.'&include=stats');
         $json_times = $response->json();
         
@@ -76,13 +78,20 @@ class DistritoController extends Controller
         $json_partidas = $response->json();
 
         $arrTimes = [];
+
         foreach ($json_times['data'] as $time){
           
             $sum = 0;
-            $num_partidas = array_count_values( array_column( $json_partidas['data'], 'homeID') )[$time['id']];
+            if($home){
+                $num_partidas = array_count_values( array_column( $json_partidas['data'], 'homeID') )[$time['id']];
+            } else {
+                $num_partidas = array_count_values( array_column( $json_partidas['data'], 'awayID') )[$time['id']];
+            }
+            
+
             foreach ($json_partidas['data'] as $partida){
                    
-                if($partida['homeID'] === $time['id']){
+                if($home and $partida['homeID'] === $time['id']){
 
                     if($partida['homeGoalCount'] > $partida['awayGoalCount']){
                        $sum = $sum + ($stake*$partida['odds_ft_1']) - $stake;
@@ -90,20 +99,51 @@ class DistritoController extends Controller
                        $sum = $sum - $stake;
                     } 
                 } 
+
+                if(!$home and $partida['awayID'] === $time['id']){
+
+                    if($partida['awayGoalCount'] > $partida['homeGoalCount']){
+                        $sum = $sum + ($stake*$partida['odds_ft_2']) - $stake;
+                     } else {
+                        $sum = $sum - $stake;
+                     } 
+
+                }
+
             }
 
-            $record = [
-                'temporada' => $time['season'],
-                'pais' => $time['country'],
-                'imagem'=> $time['image'],
-                'equipe'=> $time['cleanName'],
-                'partidas' => $time['stats']['seasonMatchesPlayed_home'],
-                'vitorias' => $time['stats']['winPercentage_home'].'%',
-                'empates' => $time['stats']['drawPercentage_home'].'%',
-                'derrotas' => $time['stats']['losePercentage_home'].'%',
-                'lucro' => round($sum,2),
-                'roi' => round($sum/$num_partidas,2)
-            ];
+            if($home){
+
+                $record = [
+                    'temporada' => $time['season'],
+                    'pais' => $time['country'],
+                    'imagem'=> $time['image'],
+                    'equipe'=> $time['cleanName'],
+                    'partidas' => $time['stats']['seasonMatchesPlayed_home'],
+                    'vitorias' => $time['stats']['winPercentage_home'].'%',
+                    'empates' => $time['stats']['drawPercentage_home'].'%',
+                    'derrotas' => $time['stats']['losePercentage_home'].'%',
+                    'lucro' => round($sum,2),
+                    'roi' => round($sum/$num_partidas,2)
+                ];
+
+            } else {
+
+                $record = [
+                    'temporada' => $time['season'],
+                    'pais' => $time['country'],
+                    'imagem'=> $time['image'],
+                    'equipe'=> $time['cleanName'],
+                    'partidas' => $time['stats']['seasonMatchesPlayed_away'],
+                    'vitorias' => $time['stats']['winPercentage_away'].'%',
+                    'empates' => $time['stats']['drawPercentage_away'].'%',
+                    'derrotas' => $time['stats']['losePercentage_away'].'%',
+                    'lucro' => round($sum,2),
+                    'roi' => round($sum/$num_partidas,2)
+                ];
+
+            }
+            
 
             array_push($arrTimes,$record); 
 
