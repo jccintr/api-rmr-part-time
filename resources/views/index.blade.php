e96397d094f85e420be7cf29b32f6aa25ea800ff
Julio Cesar <jccintr@gmail.com>
2023-12-17 21:42:07 -0300

    incluido home page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+
+body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    background-color: #effafe;
+}
+
+</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RMR PART TIME</title>
+</head>
+<body>
+    <img style="width: 300px;" src={{ asset('logo-500.png') }} alt="Logo RMR">
+</body>
+</html>
