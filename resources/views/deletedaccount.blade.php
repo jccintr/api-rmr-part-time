ef88c4736fc769e82c5017b723d328fc7fd7c9ab
Julio Cesar <jccintr@gmail.com>
2024-01-29 17:22:18 -0300

    alterada view deletedaccount

--- resources/views/deletedaccount.blade.php
+++ resources/views/deletedaccount.blade.php
@@ -5,10 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RMR Part Time</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+      .borda {
+        border: 1px solid gray;
+        border-radius: 15px;
+      }  
+      @media screen and (max-width: 767px) {
+        .borda {
+          border:0px solid;
+        }
+      }
+      
+    </style>
 </head>
 <body style="background-color: #effafe;">
     <div class="d-flex flex-column vh-100 justify-content-center align-items-center">
-        <div class="d-flex p-5 flex-column align-items-center justify-content-center border border-dark-subtle rounded-4 shadow" >
+        <div class="d-flex p-5 flex-column align-items-center justify-content-center borda" >
             <img style="width: 200px;" src={{ asset('logo-500.png') }} alt="Logo RMR">
             <p class="text-center fs-2">RMR Part Time</p>
             <p class="text-center fs-4">A sua conta foi excluída com sucesso !</p>
